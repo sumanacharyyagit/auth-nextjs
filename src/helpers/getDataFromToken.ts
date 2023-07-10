b9593d75230ea0c0e272c63a23ac7f9420b42655
sumanacharyyagit <sumanacharyya999@gmail.com>
2023-07-11 01:07:38 +0530

    added lupdates

---
+++ src/helpers/getDataFromToken.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const getDataFromToken = (request: NextRequest) => {
+    try {
+    } catch ("error: "er) {}
+};
